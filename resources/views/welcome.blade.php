267b5cdeeb28aa8643d5a73d737bfd5081927815
phyzerbert <xian1017@outlook.com>
2019-05-20 10:03:43 +0800

    upload source

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Bolton IT Help Desk</title>
+    <title>Consultant System</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
